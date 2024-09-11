7b52034796a89cb88b30976aa38a74d995013994
shubhangi24kashid <kashidshubhangi24@gmail.com>
2024-09-11 15:26:42 +0530

    Made it more responsive

--- index.js
+++ index.js
@@ -31,3 +31,12 @@ function typeEffect() {
 
 // Start the typing effect
 typeEffect();
+
+function toggleNavbar() {
+    var navbar = document.getElementById("myNavbar");
+    if (navbar.className === "navbar") {
+        navbar.className += " responsive";
+    } else {
+        navbar.className = "navbar";
+    }
+}
